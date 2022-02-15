3c5c965ff63f9c0964971d3f811b95efb9fcec2d
Maciej Olszański <olszanskimaciej@wp.pl>
2022-02-15 21:05:03 +0100

    add custom registration form fix bug - not posting when logging in or registering

--- users/views.py
+++ users/views.py
@@ -1,18 +1,22 @@
+from django import forms
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
+
+from users.forms import CustomUserCreationForm
+from .forms import CustomUserCreationForm
 
 def register(request):
     '''Register a new user'''
     if request.method != 'POST':
         # display empty form
-        form = UserCreationForm()
+        form = CustomUserCreationForm()
     else:
         # proccess filled form
-        form = UserCreationForm(data=request.POST)
+        form = CustomUserCreationForm(data=request.POST)
         if form.is_valid():
             new_user = form.save()
-            login(request, new_user,)
+            login(request, new_user)
             return redirect('budget:index')
 
     context = {'form': form}
